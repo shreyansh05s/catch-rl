2d2a32304f48a8d64a45a4b117ff84e1af074426
achris06 <tonia-c@hotmail.com>
2023-04-30 18:53:58 +0200

    Update newnew.py

--- newnew.py
+++ newnew.py
@@ -5,46 +5,91 @@ import gym
 import argparse
 from catch import Catch
 import os
+import torch.nn.functional as F
+from torch.distributions import Categorical
 
 #MODELS
+# class Policy(nn.Module):
+#     def __init__(self, input_size, hidden_size, output_size):
+#         super(Policy, self).__init__()
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.fc2 = nn.Linear(hidden_size, hidden_size)
+#         self.fc3 = nn.Linear(hidden_size, output_size)
+#         self.softmax = nn.Softmax(dim=-1)
+        
+#     def forward(self, x):
+#         x = torch.relu(self.fc1(x))
+#         x = torch.relu(self.fc2(x))
+#         x = self.softmax(self.fc3(x))
+#         return x
+
+# # Define the value network
+# class Value(nn.Module):
+#     def __init__(self, input_size, hidden_size):
+#         super(Value, self).__init__()
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.fc2 = nn.Linear(hidden_size, hidden_size)
+#         self.fc3 = nn.Linear(hidden_size, 1)
+        
+#     def forward(self, x):
+#         x = torch.relu(self.fc1(x))
+#         x = torch.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+
 class Policy(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    """
+    implements both actor and critic in one model
+    """
+
+    def __init__(self):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax(dim=-1)
+        self.conv1 = nn.Conv2d(7, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 7 * 2, 128)
+
+        # actor's layer
+        self.action_head = nn.Linear(128, 3)
+
+        # critic's layer
+        self.value_head = nn.Linear(128, 1)
+
+        # action & reward buffer
+        self.saved_actions = []
+        self.rewards = []
 
     def forward(self, x):
+        """
+        forward of both actor and critic
+        """
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.reshape(x.size(0), -1) # Flatten the tensor
         x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.softmax(self.fc3(x))
-        return x
+        
 
-# Define the value network
+        # actor: choses action to take from state s_t
+        # by returning probability of each action
+        action_prob = F.softmax(self.action_head(x), dim=-1)
 
+        # critic: evaluates being in the state s_t
+        state_values = self.value_head(x)
 
-class Value(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(Value, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, 1)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
+        # return values for both actor and critic as a tuple of 2 values:
+        # 1. a list with the probability of each action over the action space
+        # 2. the value from state s_t
+        return action_prob, state_values
 
 class ACagent:
     
     def __init__(self, input_size, output_size, hidden_size, gamma, lr, depth, trace_length, entropy_strength, device, use_baseline=False, use_bootstrapping=False):
-        self.actor = Policy(input_size, output_size, hidden_size).to(device)
-        self.critic = Value(input_size, hidden_size).to(device)
-        self.optimizer_actor = optim.Adam(self.actor.parameters(), lr=lr)
-        self.optimizer_critic = optim.Adam(self.critic.parameters(), lr=lr)
+        # self.actor = Policy(input_size, output_size, hidden_size).to(device)
+        # self.critic = Value(input_size, hidden_size).to(device)
+        self.policy = Policy().to(device)
+
+        # self.optimizer_actor = optim.Adam(self.actor.parameters(), lr=lr)
+        # self.optimizer_critic = optim.Adam(self.critic.parameters(), lr=lr)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=lr)
 
         self.gamma = gamma
         self.device = device
@@ -56,10 +101,14 @@ class ACagent:
 
     def select_action(self, state):
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-        probs = self.actor(state)
-        m = torch.distributions.Categorical(probs)
-        action = m.sample()
-        return action.item(), m.log_prob(action)
+        print(state)
+
+        # probs = self.actor(state)
+        probs = self.policy(state)[0]
+
+        dist = Categorical(probs)
+        action = dist.sample()
+        return action, dist
 
     def sample_trace(self, env):
         done = False
@@ -75,32 +124,45 @@ class ACagent:
         return trace, reward
 
     def update(self, trace):
-        actor_loss = 0
-        critic_loss = 0
+        actor_loss = []
+        critic_loss = []
         R = 0
         n = self.depth
         for i in range(len(trace)-1):
-            V = self.critic.forward(torch.from_numpy(trace[i+n][0]).float().unsqueeze(0).to(self.device))
+
+            # V = self.critic.forward(torch.from_numpy(trace[i+n][0]).float().unsqueeze(0).to(self.device))
+            V = self.policy.forward(torch.from_numpy(trace[i+n][0]).float().unsqueeze(0).to(self.device))[1]
+
             Q_n = sum([trace[i + k][2] for k in range(n)]) + V
-            v_pred = self.critic.forward(trace[i][0], self.device)
+
+            # v_pred = self.critic.forward(trace[i][0], self.device)
+            v_pred = self.policy.forward(trace[i][0]).to(self.device)[1]
 
             if self.use_baseline:
                 advantage = Q_n - v_pred
             else:
                 advantage = Q_n
 
-            actor_loss = actor_loss - trace[i][3] * advantage.detach()
-            actor_loss -= self.entropy_weight * trace[i][3].entropy()
-            critic_loss = critic_loss + nn.MSELoss()(v_pred, Q_n)
+            # actor_loss = trace[i][3] * advantage.detach()
+            actor_loss.append(trace[i][3] * advantage.detach() - self.entropy_weight * trace[i][3].entropy())
+            critic_loss.append(nn.MSELoss()(v_pred, Q_n))
+
+        actor_loss = torch.stack(actor_loss).sum()
+        critic_loss = torch.stack(critic_loss).sum()
 
         #update the actor and critic
-        self.optimizer_actor.zero_grad()
-        actor_loss.backward()
-        self.optimizer_actor.step()
+        # self.optimizer_actor.zero_grad()
+        # actor_loss.backward()
+        # self.optimizer_actor.step()
 
-        self.optimizer_critic.zero_grad()
+        # self.optimizer_critic.zero_grad()
+        # critic_loss.backward()
+        # self.optimizer_critic.step()
+
+        self.optimizer.zero_grad()
+        actor_loss.backward()
         critic_loss.backward()
-        self.optimizer_critic.step()
+        self.optimizer.step()
 
     def train(self, env, num_episodes):
         for i in range(num_episodes):
@@ -114,7 +176,7 @@ class ACagent:
             state = env.reset()
             reward = 0
             while not done:
-                env.render()
+                # env.render()
                 action, _ = self.select_action(state)
                 state, r, done, _ = env.step(action)
                 reward += r
@@ -123,7 +185,7 @@ class ACagent:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", default="test", type=str)
+    parser.add_argument("--mode", default="train", type=str)
     parser.add_argument("--env", default="Catch", type=str)
     parser.add_argument("--hidden_size", default=128, type=int)
     parser.add_argument("--gamma", default=0.99, type=float)
@@ -140,8 +202,8 @@ if __name__ == "__main__":
     parser.add_argument("--depth", default=10, type=int)
     args = parser.parse_args()
 
-    env = Catch()
-    agent = ACagent(env.observation_space.shape[0] * env.observation_space.shape[1] * env.observation_space.shape[2], env.action_space.n, args.hidden_size, args.gamma, args.lr, args.depth, args.trace_length, args.entropy_strength, args.device, args.use_baseline, args.use_bootstrapping)
+    env = Catch(rows=7, columns=7, speed=1.0, max_steps=250, max_misses=10, observation_type='pixel', seed=None)
+    agent = ACagent(env.observation_space.shape[0] * env.observation_space.shape[1], env.action_space.n, args.hidden_size, args.gamma, args.lr, args.depth, args.trace_length, args.entropy_strength, args.device, args.use_baseline, args.use_bootstrapping)
 
     if args.mode == "train":
         agent.train(env, args.num_episodes)
