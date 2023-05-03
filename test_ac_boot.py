193c171c690cb771e4778502b542d0a083758da8
achris06 <tonia-c@hotmail.com>
2023-05-03 18:16:37 +0200

    Update test_ac_boot.py

--- test_ac_boot.py
+++ test_ac_boot.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # env.reset(seed=seed)
 # torch.manual_seed(seed)
 
-
+# create a tuple to store the information about our transitions
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value', 'entropy'])
 
 
@@ -24,17 +24,17 @@ class Policy(nn.Module):
     implements both actor and critic in one model
     """
 
-    def __init__(self):
+    def __init__(self, input_size=7, hidden_size1=16, hidden_size2=32, hidden_size3=128, actor_output=3, critic_output=1):
         super(Policy, self).__init__()
-        self.conv1 = nn.Conv2d(7, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 2, 128)
+        self.conv1 = nn.Conv2d(input_size, hidden_size1, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(hidden_size1, hidden_size2, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(hidden_size2 * input_size * 2, hidden_size3)
 
         # actor's layer
-        self.action_head = nn.Linear(128, 3)
+        self.action_head = nn.Linear(hidden_size3, actor_output)
 
         # critic's layer
-        self.value_head = nn.Linear(128, 1)
+        self.value_head = nn.Linear(hidden_size3, critic_output)
 
         # action & reward buffer
         self.saved_actions = []
@@ -62,27 +62,18 @@ class Policy(nn.Module):
         # 2. the value from state s_t
         return action_prob, state_values
 
-
-model = Policy()
+model = Policy(input_size=7, hidden_size1=16, hidden_size2=32, hidden_size3=128, actor_output=3, critic_output=1)
 optimizer = optim.Adam(model.parameters(), lr=1e-2)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.99)
 # scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
 eps = np.finfo(np.float32).eps.item()
 
-
 def select_action(state):
     state = torch.FloatTensor(state).unsqueeze(0)
-    # state = state.unsqueeze(0)
-    # print(state.shape)
-    # print(state)
     probs, state_value = model(state)
-    # print(probs)
 
     # create a categorical distribution over the list of probabilities of actions
     m = Categorical(probs)
-    
-    # add entropy to encourage exploration
-    
 
     # and sample an action using the distribution
     action = m.sample()
@@ -90,11 +81,11 @@ def select_action(state):
     # save to action buffer
     model.saved_actions.append(SavedAction(m.log_prob(action), state_value, m.entropy().item()))
 
-    # the action to take (left or right)
+    # the action to take (left, right, or stay idle)
     return action.item()
 
 
-def finish_episode(gamma, entropy_weight, baseline=True, entropy_regularization=False, normalize_returns=True, bootstrap=True, bootstrap_state=None, done=False):
+def update(gamma, entropy_weight, baseline=True, entropy_regularization=False, normalize_returns=True, bootstrap=False, bootstrap_state=None, done=False):
     """
     Training code. Calculates actor and critic loss and performs backprop.
     """
@@ -103,11 +94,8 @@ def finish_episode(gamma, entropy_weight, baseline=True, entropy_regularization=
     policy_losses = []  # list to save actor (policy) loss
     value_losses = []  # list to save critic (value) loss
     returns = []  # list to save the true values
-    
-    # use bootstrapping to estimate the n step return
-    
 
-    # calculate the true value using rewards returned from the environment
+    # use or not of bootstraping to estimate the n step return
     if bootstrap:
         for i, r in enumerate(model.rewards[::-1]):
             if i == 0:
@@ -117,39 +105,34 @@ def finish_episode(gamma, entropy_weight, baseline=True, entropy_regularization=
             returns.insert(0, R)
     else:
         for r in model.rewards[::-1]:
-        # calculate the discounted value
+            # calculate the discounted value
             R = r + gamma * R
             returns.insert(0, R)
 
     returns = torch.tensor(returns)
     
-    # normalize the true values (this is not 100% necessary)
-    # show experimentally that this helps to stabilize training
+    # normalize the returns to stabilize training
     if normalize_returns:
         returns = (returns - returns.mean()) / (returns.std() + eps)
-        
-    # implement without bootstrapping where bootstrap is the value of the last state
-    #if bootstrap:
         
 
     for (log_prob, value, entropy), R in zip(saved_actions, returns):
 
+        # calculate advantage using or not baseline subtraction
         if baseline:
             advantage = R - value.item()
 
         else:
             advantage = R
         
-        # add entropy to encourage exploration
+        # add entropy to encourage exploration multiplied by a weight factor to control its strength
         if entropy_regularization:
-            log_prob += entropy * entropy_weight
+            log_prob += entropy * entropy_weight 
         
-        # calculate actor (policy) loss        
+        # calculate policy loss        
         policy_losses.append(-log_prob * advantage)
 
-        # calculate critic (value) loss using L1 smooth loss
-        # value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
-        
+        # calculate value loss 
         value_losses.append(F.mse_loss(value, torch.tensor([R])))
 
     # reset gradients
@@ -158,7 +141,7 @@ def finish_episode(gamma, entropy_weight, baseline=True, entropy_regularization=
     # sum up all the values of policy_losses and value_losses
     loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
 
-    # perform backprop
+    # perform backpropagation
     loss.backward()
     optimizer.step()
 
@@ -168,6 +151,7 @@ def finish_episode(gamma, entropy_weight, baseline=True, entropy_regularization=
 
 
 def main():
+    # Training settings
     parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
     parser.add_argument('--gamma', type=float, default=0.85, metavar='G',
                         help='discount factor (default: 0.99)')
@@ -181,21 +165,26 @@ def main():
                         help='number of steps to train (default: 1000)')
     parser.add_argument('--wandb_project', type=str, default='catch-rl', help='Wandb project name')
     parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
-    parser.add_argument('--entropy_weight', type=int, default=0.01, help='strength of entropy regularization')
+    parser.add_argument('--entropy_weight', type=float, default=0.01, help='strength of entropy regularization')
+    parser.add_argument('--baseline', type=bool, default=True, help='use of baseline subtraction')
+    parser.add_argument('--bootstrap', type=bool, default=True, help='use of bootstrapping')
+    parser.add_argument('--entropy_regularization', type=bool, default=True, help='use of entropy regularization')
+    parser.add_argument('--normalize_returns', type=bool, default=True, help='normalise returns')
     args = parser.parse_args()
 
+    
+    running_reward = 10
+    num_episodes = 1000
     observation_type = 'pixel'
 
     wandb.init(mode='disabled')
     # wandb.init(project=args.wandb_project, config=args)
 
+    # set up environment
     env = Catch(rows=7, columns=7, speed=1.0, max_steps=250,
             max_misses=10, observation_type=observation_type, seed=None)
 
-    bootstrap = True
-    
-    running_reward = 10
-    num_episodes = 1000
+
     # run infinitely many episodes
     for i_episode in range(num_episodes):
 
@@ -203,10 +192,9 @@ def main():
         state = env.reset()
         ep_reward = 0
 
-        # for each episode, only run 9999 steps so that we don't
-        # infinite loop while learning
         done = False
         while not done and i_episode <= num_episodes:
+
             # select action from policy
             action = select_action(state)
 
@@ -218,13 +206,15 @@ def main():
 
             model.rewards.append(reward)
             ep_reward += reward
-            if done and not bootstrap:
-                finish_episode(args.gamma, args.entropy_weight, baseline=True, entropy_regularization=True, normalize_returns=True, bootstrap=True, bootstrap_state=None, done=True)
-            elif bootstrap and len(model.rewards) >= args.n_steps:
-                # update the policy after every n steps
+
+            if done and not args.bootstrap:
+                update(args.gamma, args.entropy_weight, args.baseline, args.entropy_regularization, args.normalize_returns, args.bootstrap, bootstrap_state=None, done=True)
+
+            elif args.bootstrap and len(model.rewards) >= args.n_steps:
                 
+                # update the policy after every n steps
                 bootstrap_state = torch.FloatTensor(next_state).unsqueeze(0)
-                finish_episode(args.gamma, args.entropy_weight, baseline=True, entropy_regularization=True, normalize_returns=True, bootstrap=True, bootstrap_state=bootstrap_state, done=False)
+                update(args.gamma, args.entropy_weight, args.baseline, args.entropy_regularization, args.normalize_returns, args.bootstrap, bootstrap_state=bootstrap_state, done=False)
                 
                 # reset the rewards and action buffer to have only the last n steps
                 # model.rewards = model.rewards[-(n_steps-1):]
@@ -237,9 +227,9 @@ def main():
         # update cumulative reward
         running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
 
-        # perform backprop
-        
+        # update learning rate         
         scheduler.step()
+
         # log results
         if i_episode % args.log_interval == 0:
             print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}, lr: {:.5f}'.format(
